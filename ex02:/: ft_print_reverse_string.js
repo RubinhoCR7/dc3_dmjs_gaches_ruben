4ba228173e3d90f7dabbd4816e6b77b2ec793a4e
RubinhoCR7 <99280508+RubinhoCR7@users.noreply.github.com>
2023-02-04 23:32:56 +0100

    ex02/

---
+++ ex02:/: ft_print_reverse_string.js
@@ -0,0 +1,5 @@
+function petitReversStringCommeOnLesAimes(str) {
+  return str.split('').reverse().join('');
+}
+
+console.log(petitReversStringCommeOnLesAimes("Bonjour Rida, petit dunk à la Lebron"));
